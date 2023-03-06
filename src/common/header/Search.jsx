dab6083d338aaee1fc466e0bf8456c37ab90d55d
= <=>
2023-03-06 15:04:51 +0000

    first commit

---
+++ src/common/header/Search.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import logo from "../../components/assets/images/logo.svg"
+import { Link } from "react-router-dom"
+
+const Search = ({ CartItem }) => {
+  // fixed Header
+  window.addEventListener("scroll", function () {
+    const search = document.querySelector(".search")
+    search.classList.toggle("active", window.scrollY > 100)
+  })
+
+  return (
+    <>
+      <section className='search'>
+        <div className='container c_flex'>
+          <div className='logo width '>
+            <img src="kimologo" alt='' />
+            {/* <h2>Kimo</h2> */}
+          </div>
+
+          <div className='search-box f_flex'>
+           
+            <input type='text' placeholder='All Category' />
+            <span> <i className='fa fa-search'></i>Search</span>
+          </div>
+
+          <div className='icon f_flex width'>
+            <i className='fa fa-user icon-circle'></i>
+            <div className='cart'>
+              <Link to='/cart'>
+                <i className='fa fa-shopping-cart icon-circle'></i>
+                <span>{CartItem.length === 0 ? "" : CartItem.length}</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Search
